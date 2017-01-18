e784602f6545a90e23228f2c1f73d19295edef27
Fedor Nezhivoi <gyzerok@gmail.com>
2017-01-18 17:13:01 +0200

    implement new api

--- modules/effects.js
+++ modules/effects.js
@@ -1,136 +1,43 @@
-import { throwInvariant, flatten } from './utils';
-
 const isEffectSymbol = Symbol('isEffect');
 
 const effectTypes = {
-  PROMISE: 'PROMISE',
-  CALL: 'CALL',
-  BATCH: 'BATCH',
-  CONSTANT: 'CONSTANT',
-  NONE: 'NONE',
-  LIFT: 'LIFT',
+  LEAF: 'LEAF',
+  MAP: 'MAP',
 };
 
 
-/**
-* Runs an effect and returns the Promise for its completion.
-* @param {Object} effect The effect to convert to a Promise.
-* @returns {Promise} The converted effect Promise.
-*/
-export function effectToPromise(effect) {
+export function toPromise(effect) {
   if(process.env.NODE_ENV === 'development') {
-    throwInvariant(
-      isEffect(effect),
-      'Given effect is not an effect instance.'
-    );
+    if (!isEffect(effect)) {
+      throw new Error('Given effect is not an effect instance.');
+    }
   }
 
   switch (effect.type) {
-    case effectTypes.PROMISE:
-      return effect.factory(...effect.args).then((action) => [action]);
-    case effectTypes.CALL:
-      return Promise.resolve([effect.factory(...effect.args)]);
-    case effectTypes.BATCH:
-      return Promise.all(effect.effects.map(effectToPromise)).then(flatten);
-    case effectTypes.CONSTANT:
-      return Promise.resolve([effect.action]);
-    case effectTypes.NONE:
-      return Promise.resolve([]);
-    case effectTypes.LIFT:
-      return effectToPromise(effect.effect).then((actions) =>
-        actions.map((action) => effect.factory(...effect.args, action))
-      );
+    case effectTypes.LEAF:
+      return effect.promiseCreator();
+    case effectTypes.MAP:
+      return toPromise(effect.effect).then((action) => effect.tagger(action));
   }
 }
 
-/**
- * Determines if the object was created with an effect creator.
- * @param {Object} object The object to inspect.
- * @returns {Boolean} Whether the object is an effect.
- */
 export function isEffect(object) {
   return object ? object[isEffectSymbol] : false;
 }
 
-/**
- * Determines id the effect object is of type none
- * @param {Object} The object to inspect.
- * @returns {Boolean} Whether the object is a none effect.
- */
-export function isNone(object) {
-  return object ? object.type === effectTypes.NONE : false;
-}
-
-/**
- * Creates a noop effect.
- * @returns {Object} An effect of type NONE, essentially a no-op.
- */
-export function none() {
+export function fromLazyPromise(promiseCreator) {
   return {
-    type: effectTypes.NONE,
+    promiseCreator,
+    type: effectTypes.LEAF,
     [isEffectSymbol]: true
   };
 }
 
-/**
- * Creates an effect for a function that returns a Promise.
- * @param {Function} factory The function to invoke with the given args that returns a Promise for an action.
- * @returns {Object} The wrapped effect of type PROMISE.
- */
-export function promise(factory, ...args) {
-  return {
-    factory,
-    args,
-    type: effectTypes.PROMISE,
-    [isEffectSymbol]: true
-  };
-}
-
-/**
- * Creates an effect for a function that returns an action.
- * @param {Function} factory The function to invoke with the given args that returns an action.
- * @returns {Object} The wrapped effect of type CALL.
- */
-export function call(factory, ...args) {
-  return {
-    factory,
-    args,
-    type: effectTypes.CALL,
-    [isEffectSymbol]: true
-  };
-}
-
-/**
- * Composes an array of effects together.
- */
-export function batch(effects) {
-  return {
-    effects,
-    type: effectTypes.BATCH,
-    [isEffectSymbol]: true
-  };
-}
-
-/**
- * Creates an effect for an already-available action.
- */
-export function constant(action) {
-  return {
-    action,
-    type: effectTypes.CONSTANT,
-    [isEffectSymbol]: true
-  };
-}
-
-/**
- * Transform the return type of a bunch of `Effects`. This is primarily useful for adding tags to route `Actions` to the right place
- */
-export function lift(effect, factory, ...args) {
+export function map(effect, tagger) {
   return {
     effect,
-    factory,
-    args,
-    type: effectTypes.LIFT,
+    tagger,
+    type: effectTypes.MAP,
     [isEffectSymbol]: true
   };
 }
--- modules/errors.js
+++ modules/errors.js
@@ -1,29 +1,40 @@
-export const loopPromiseCaughtError = (originalActionType) =>
+export const loopPromiseCaughtError =
 `
-loop Promise caught when returned from action of type ${originalActionType}.
-loop Promises must not throw!
+Promise inside your effect caught when returned from reducer.
+Promise inside your effects must not throw!
 
 Did you forget to do one of the following?
 
-- Call \`.catch\` on a Promise in a function passed to \`Effects.promise\`
+- Call \`.catch\` on a Promise in a function passed to \`Effects.fromLazyPromise\`
 
-  const asyncEffect = (val) => {
-    return api.doStuff(val)
-      .then((stuff) => Actions.success(stuff))
-      .catch((error) => Actions.failure(error)); // <-- You have to do this!
-  };
+  const effect = Effects.fromLazyPromise(() => {
+    return new Promise((resolve, reject) => {
+      throw 'Something wrong happened!';
+    })
+    .catch(() => {         // <-- You forgot to do this!
+      return {
+        type: 'FAILURE',
+      };
+    });
+  });
 
-- Return an action from a \`.catch\` callback
+- Return an action from \`.then\` or \`.catch\` callback
 
-  const asyncEffect = (val) => {
-    return api.doStuff(val)
-      .then((stuff) => {
-        return Actions.success(stuff); // <-- Make sure to return here!
-      })
-      .catch((error) => {
-        return Actions.failure(error): // <-- And return here!
-      });
-  };
+  const effect = Effects.fromLazyPromise(() => {
+    return new Promise((resolve, reject) => {
+      /* Some side-effectful thingy */
+    })
+    .then(() => {          // <-- Make sure to return action here!
+      return {
+        type: 'SUCCESS',
+      };
+    })
+    .catch(() => {         // <-- And return action here!
+      return {
+        type: 'FAILURE',
+      };
+    });
+  });
 
-Don't see the problem here? Please report the issue at <https://github.com/raisemarketplace/redux-loop/issues/new>
+Don't see the problem here? Please report the issue at <https://github.com/redux-loop/redux-loop/issues/new>
 `;
