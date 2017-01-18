5441022cad9df387ae3f52308689e9f5f525e5ba
Fedor Nezhivoi <gyzerok@gmail.com>
2017-01-18 17:13:01 +0200

    implement new api

--- modules/index.js
+++ modules/index.js
@@ -1,45 +1,60 @@
-import {
-  loop,
-  liftState,
-  getModel,
-  getEffect,
-  isLoop
-} from './loop';
+import * as Redux from 'redux';
 
-import {
-  batch,
-  none,
-  constant,
-  promise,
-  call,
-  lift,
-} from './effects';
-
-import {
-  install,
-} from './install';
-
-import {
-  combineReducers,
-} from './combineReducers';
-
-
-const Effects = {
-  constant,
-  promise,
-  call,
-  batch,
-  none,
-  lift,
-};
+import { loopPromiseCaughtError } from './errors';
+import * as Effects from './effects';
 
 export {
-  combineReducers,
   Effects,
-  install,
-  loop,
-  isLoop,
-  liftState,
-  getModel,
-  getEffect,
 };
+
+export function createStore(reducer, initialModel, enhancer) {
+  return (function () {
+    let queue = []
+
+    function liftReducer(reducer) {
+      return (state, action) => {
+        const ret = reducer(state, action)
+
+        ret.effects.forEach((effect) => queue.push(effect))
+
+        return ret.state
+      }
+    }
+
+    const store = Redux.createStore(
+      liftReducer(reducer),
+      initialModel.state,
+      enhancer,
+    )
+
+    function executeEffects(callback, effects) {
+      return effects.map(effect =>
+        Effects
+          .toPromise(effect)
+          .then(callback)
+          .catch(err => {
+            throw loopPromiseCaughtError;
+            console.error(err);
+          })
+      );
+    }
+
+    function enhancedDispatch(action) {
+      store.dispatch(action)
+
+      const currentQueue = queue;
+      queue = []
+
+      return Promise.all(
+        executeEffects(enhancedDispatch, currentQueue)
+      );
+    }
+
+    executeEffects(enhancedDispatch, initialModel.effects)
+
+    return {
+      ...store,
+      dispatch: enhancedDispatch,
+    };
+  })();
+}
