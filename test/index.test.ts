2e01f64a7b16938c31e2850c849a2f85043c1309
Fedor Nezhivoi <gyzerok@gmail.com>
2017-02-21 17:44:51 +0200

    update tests to use mocha and chai

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,124 +1,132 @@
-import * as test from 'tape';
+import { expect } from 'chai';
 import * as Redux from 'redux';
 
 import { createStore, Effects } from '../modules';
 
 
-test('store state is setted to one from initial loop', (t) => {
-  t.plan(1);
+const EXPECT_TIMEOUT = 20;
 
-  const init = {
-    state: 1,
-    effects: []
-  };
+describe('Suit', () => {
+  describe('createStore', () => {
+    it('should set store to inital state provided', () => {
+      const init = {
+        state: 1,
+        effects: []
+      };
 
-  const reducer = (state: number, action: Redux.Action) => {
-    return { state, effects: [] };
-  }
-
-  const store = createStore(reducer, init);
-
-  t.equal(store.getState(), 1);
-});
-
-
-test('effects within initial loop get dispatched', (t) => {
-  t.plan(1);
-
-  const ACTION = 'ACTION';
-
-  const init = {
-    state: 1,
-    effects: [
-      Effects.fromLazyPromise(() => 
-        Promise
-          .resolve()
-          .then(() => ({ type: ACTION, arg: 3 }))
-      )
-    ]
-  };
-
-  const reducer = (state: number, action: Redux.Action) => {
-    switch (action.type) {
-      case ACTION:
-        t.ok(true);
+      const reducer = (state: number, action: Redux.Action) => {
         return { state, effects: [] };
-      default:
-        return { state, effects: [] };
-    }
-  }
+      }
 
-  createStore(reducer, init);
-});
+      const store = createStore(reducer, init);
 
-
-test('effects returned by the loop get dispatched', (t) => {
-  t.plan(2);
-
-  const FIRST_ACTION = 'FIRST_ACTION';
-  const SECOND_ACTION = 'SECOND_ACTION';
-
-  const init = {
-    state: 1,
-    effects: [
-      Effects.fromLazyPromise(() => 
-        Promise
-          .resolve()
-          .then(() => ({ type: FIRST_ACTION }))
-      )
-    ]
-  };
-
-  const reducer = (state: number, action: Redux.Action) => {
-    switch (action.type) {
-      case FIRST_ACTION:
-        t.ok(true);
-        return {
-          state,
-          effects: [
-            Effects.fromLazyPromise(() => 
-              Promise
-                .resolve()
-                .then(() => ({ type: SECOND_ACTION }))
-            )
-          ]
-        };
-      case SECOND_ACTION:
-        t.ok(true);
-        return {
-          state,
-          effects: []
-        };
-      default:
-        return { state, effects: [] };
-    }
-  }
-
-  createStore(reducer, init);
-});
-
-
-test('Effects#map works', (t) => {
-  t.plan(1);
-
-  const childEffect = Effects.fromLazyPromise(() => Promise.resolve({ type: 'child' }));
-  const parentEffect = childEffect.map(action => ({
-    type: 'parent',
-    nested: action,
-  }))
-
-  parentEffect
-    .toPromise()
-    .then((action) => {
-      t.deepEqual(
-        action,
-        {
-          type: 'parent',
-          nested: {
-            type: 'child',
-          },
-        },
-        'the action takes the proper shape for a lifted action'
-      );
+      expect(store.getState()).to.be.equal(1);
     });
+
+    it('should dispatch effects within inital state', done => {
+      const ACTION = 'ACTION';
+
+      const init = {
+        state: 1,
+        effects: [
+          Effects.fromLazyPromise(() => 
+            Promise
+              .resolve()
+              .then(() => ({ type: ACTION, arg: 3 }))
+          )
+        ]
+      };
+
+      const reducer = (state: number, action: Redux.Action) => {
+        switch (action.type) {
+          case ACTION:
+            return { state: state + 10, effects: [] };
+          default:
+            return { state, effects: [] };
+        }
+      }
+
+      const store = createStore(reducer, init);
+
+      setTimeout(() => {
+        expect(store.getState()).to.be.equal(11);
+        done();
+      }, EXPECT_TIMEOUT);
+    });
+
+    it('should dispatch effects returned by reducer', done => {
+      const FIRST_ACTION = 'FIRST_ACTION';
+      const SECOND_ACTION = 'SECOND_ACTION';
+
+      const init = {
+        state: 1,
+        effects: [
+          Effects.fromLazyPromise(() => 
+            Promise
+              .resolve()
+              .then(() => ({ type: FIRST_ACTION }))
+          )
+        ]
+      };
+
+      const reducer = (state: number, action: Redux.Action) => {
+        switch (action.type) {
+          case FIRST_ACTION:
+            return {
+              state: state + 5,
+              effects: [
+                Effects.fromLazyPromise(() => 
+                  Promise
+                    .resolve()
+                    .then(() => ({ type: SECOND_ACTION }))
+                )
+              ]
+            };
+          case SECOND_ACTION:
+            return {
+              state: state * 3,
+              effects: []
+            };
+          default:
+            return { state, effects: [] };
+        }
+      }
+
+      const store = createStore(reducer, init);
+
+      setTimeout(() => {
+        expect(store.getState()).to.be.equal(18);
+        done();
+      }, EXPECT_TIMEOUT);
+    });
+  });
+
+  describe('Effects', () => {
+    it('#toPromise should throw if wrongly created', () => {
+      const effect = Effects.fromLazyPromise(() => 'hello' as any);
+
+      expect(effect.toPromise).to.throw(Error);
+    });
+
+    it('#map should transform actions', () => {
+      const childEffect = Effects.fromLazyPromise(() => Promise.resolve({ type: 'child' }));
+      const parentEffect = childEffect.map(action => ({
+        type: 'parent',
+        nested: action,
+      }))
+
+      return parentEffect
+        .toPromise()
+        .then((action) => {
+          expect(action).to.be.deep.equal(
+            {
+              type: 'parent',
+              nested: {
+                type: 'child',
+              },
+            },
+          );
+        });
+    });
+  });
 });
