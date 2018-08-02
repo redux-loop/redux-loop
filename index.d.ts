ddefe8f9e8f08712d1370464b9fbbd10906c0b05
stereobooster <stereobooster@gmail.com>
2018-08-02 16:36:34 +0200

    Add documentation to TS defintion

--- index.d.ts
+++ index.d.ts
@@ -85,13 +85,44 @@ declare function loop<S, A extends Action>(
 ): Loop<S, A>;
 
 declare namespace Cmd {
+  /**
+   * A symbol that can be passed to a cmd object as an arg (from a reducer)
+   * that will be replaced at the time the function is called with
+   * the `dispatch` method from the store.
+   */
   export const dispatch: symbol;
+  /**
+   * A symbol that can be passed to a cmd object as an arg (from a reducer)
+   * that will be replaced at the time the function is called with
+   * the `getState()` method from the store.
+   */
   export const getState: symbol;
+  /**
+   * is a no-op effect that you can use for convenience when building custom
+   * effect creators from the ones provided. Since it does not resolve to
+   * an action it doesn't cause any side effects to actually occur.
+   */
   export const none: NoneCmd;
+  /**
+   * allows you to schedule a plain action object for dispatch after
+   * the current dispatch is complete. It can be useful for initiating
+   * multiple sequences that run in parallel but don't need to communicate
+   * or complete at the same time.
+   */
   export function action<A extends Action>(action: A): ActionCmd<A>;
   export function batch<A extends Action>(cmds: CmdType<A>[]): BatchCmd<A>;
   export function sequence<A extends Action>(cmds: CmdType<A>[]): SequenceCmd<A>;
 
+  /**
+   * allows you to group cmd objects as a single cmd to be run all together.
+   * Use the options to choose when you want the individual cmd objects to run and
+   * when the resulting actions are dispatched. The default behavior is to run
+   * each cmd object simultaneously and dispatch the results as soon as possible.
+   *
+   * @param cmds an array of cmd objects returned by any of the other cmd functions, or even nested calls to `Cmd.list`
+   * @param options.sequence By default, asynchronous cmd objects all run immediately and in parallel. If sequence is true, each cmd object will wait for the previous cmd object to resolve before starting. Note: this does not have an effect if all cmd objects are synchronous.
+   * @param options.batch By default, actions from nested cmd objects will be dispatched as soon as that cmd object finishes. If batch is true, no actions will be dispatched until all of the cmd objects are resolved/finished. The actions will then be dispatched all at once in the order of the original cmd array.
+   */
   export function list<A extends Action>(
     cmds: CmdType<A>[],
     options?: {
@@ -101,12 +132,30 @@ declare namespace Cmd {
     }
   ): ListCmd<A>;
 
+  /**
+   * allows you to take an existing cmd object from a nested reducer in your state
+   * and lift it to a more general action in which the resulting action is nested.
+   * This enables you to build your reducer in a fractal-like fashion, in which all
+   * of the logic for a particular slice of your state is totally encapsulated and
+   * actions can be simply directed to the reducer for that slice.
+   */
   export function map<A extends Action, B extends Action>(
     cmd: CmdType<B>,
     tagger: (subAction: B) => A,
     args?: any[]
   ): MapCmd<A>;
 
+  /**
+   * allows you to declaratively schedule a function to be called with some arguments
+   * and dispatch actions based on the results. This allows you to represent almost
+   * any kind of runnable process to the store without sacrificing functional purity
+   * or having to encapsulate implicit state outside of your reducer. Keep in mind,
+   * functions that are handed off to the store with run() are never invoked in
+   * the reducer, only by the store during your application's runtime.
+   * You can invoke a reducer that returns a run() effect as many times as you want
+   * and always get the same result by deep-equality without triggering any side-effect
+   * function calls in the process
+   */
   export function run<A extends Action>(
     f: Function,
     options?: {
@@ -136,6 +185,10 @@ declare function reduceReducers<S, A extends Action = AnyAction>(
   ...reducers: Array<LoopReducer<S, A>>
 ): LiftedLoopReducer<S, A>;
 
+/**
+ * Automatically converts objects to loop() results. If the value was created with loop(), then the function behaves as an identity. Otherwise, it is lifted into a [any, Cmd] pair where the effect is Cmd.none. Useful for forcing reducers to always return a loop() result, even if they shortcut to just the model internally.
+ * @param state an object which may be the state of the redux store, or an existing [any, Cmd] pair created by loop().
+ */
 declare function liftState<S, A extends Action>(
   state: S | Loop<S, A>
 ): Loop<S, A>;
